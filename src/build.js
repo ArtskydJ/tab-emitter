c040b33186014c9063fc488f408a88128915cded
Joseph Dykstra <josephdykstra@gmail.com>
2015-11-02 22:32:20 -0600

    Fix build

--- src/build.js
+++ src/build.js
@@ -6,6 +6,6 @@ var jsPath = path.join(__dirname, 'index.js')
 
 var code = fs.readFileSync(jsPath, 'utf-8').replace(/\t/g, '    ')
 var html = fs.readFileSync(htmlPath, 'utf-8')
-	.replace(/<pre><code>[\s\S]*<\/code><\/pre>/, '<pre><code>' + code + '</pre></code>')
+	.replace(/<pre><code>[\s\S]*<\/code><\/pre>/, '<pre><code>' + code + '</code></pre>')
 
 fs.writeFileSync(htmlPath, html)
