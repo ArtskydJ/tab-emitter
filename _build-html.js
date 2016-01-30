1db720ffd21bb5bce1254ee2f63355ea78f1abdb
Joseph Dykstra <josephdykstra@gmail.com>
2016-01-30 17:38:46 -0600

    Redo build system
    Use uncss to remove unused css (was 25 kb, now is 4.5 kb, fixes #15)
    Write one build script for css, one for html, one for js
    
    Since files starting with underscores don't get hosted...
    Move src/build.js to _build-html.js
    Move src/index.js to _src.js

--- _build-html.js
+++ _build-html.js
@@ -1,8 +1,8 @@
 var fs = require('fs')
 var path = require('path')
 
-var htmlPath = path.join(__dirname, '..', 'index.html')
-var jsPath = path.join(__dirname, 'index.js')
+var htmlPath = path.join(__dirname,  'index.html')
+var jsPath = path.join(__dirname, '_src.js')
 
 var code = fs.readFileSync(jsPath, 'utf-8').replace(/\t/g, '    ')
 var html = fs.readFileSync(htmlPath, 'utf-8')
